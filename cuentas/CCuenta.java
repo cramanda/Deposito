d2c18781dfae8ba6c64000cf5443f0a05b244fc6
Amanda Conejero Reinaldo <amandaconejero@paucasesnovescifp.cat>
2025-02-15 22:30:41 +0100

    Subida inicial del proyecto

---
+++ cuentas/CCuenta.java
@@ -0,0 +1,73 @@
+package cuentas;
+
+public class CCuenta {
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInteres;
+
+    public CCuenta() {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        this.nombre = nom;
+        this.cuenta = cue;
+        this.saldo = sal;
+        this.tipoInteres = tipo;
+    }
+
+    public double estado() {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        }
+        saldo += cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No hay suficiente saldo");
+        }
+        saldo -= cantidad;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
+    }
+}
+
